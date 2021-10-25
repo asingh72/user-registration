f928608f0c0f07690d3f2ff79f1bc9e85fbed82c
Anurag Singh <asingh72@binghamton.edu>
2021-10-24 21:58:10 -0400

    Path variable name changed from id to user_id

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.model.Address;
@@ -23,7 +24,7 @@ public class UserController {
 	
 	
 	//Create user:: not in task list
-	@PostMapping( "/users/createUser")
+	@PostMapping( "/createUser")
 	public void createUser(@RequestBody User user) {
 		 userService.createUser(user);
 	}
@@ -48,21 +49,21 @@ public class UserController {
 	}
 	
 	//d. GetUserDetails API inputs user id and returns user details including address.
-	@GetMapping("/GetUserDetails/{id}")
-	public Object getUser(@PathVariable int id) {	
-		return userService.getUserById(id);
+	@GetMapping("/GetUserDetails/{user_id}")
+	public Object getUser(@PathVariable int user_id) {	
+		return userService.getUserById(user_id);
 	}
 	
 	//e. DeleteUser API which takes user id and marks the user as deleted.
-	@DeleteMapping("/DeleteUser/{id}")
-	public void updateUser(@PathVariable int id) {
-		userService.deleteUser(id);
+	@DeleteMapping("/DeleteUser/{user_id}")
+	public void updateUser(@PathVariable int user_id) {
+		userService.deleteUser(user_id);
 	}
 		
 	//f. UpdateAddress API which takes user id and updates user address such that the old address is archived.
-	@PutMapping("/UpdateAddress/{id}")
-	public void updateAddress(@RequestBody  Address address, @PathVariable int id) {
-		userService.updateAddress(address,id);
+	@PutMapping("/UpdateAddress/{user_id}")
+	public void updateAddress(@RequestBody  Address address, @PathVariable int user_id) {
+		userService.updateAddress(address,user_id);
 	}
 	
 }
