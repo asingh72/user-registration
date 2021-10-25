72b17f9c73f5f554fc6ec7c8972e837d23ac7294
Anurag Singh <asingh72@binghamton.edu>
2021-10-24 21:53:38 -0400

    Swagger properties change

--- src/main/java/com/app/UserRegistrationApplication.java
+++ src/main/java/com/app/UserRegistrationApplication.java
@@ -2,7 +2,11 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,6 +15,18 @@ public class UserRegistrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserRegistrationApplication.class, args);
+		
+	
+	}
+	
+	@Bean
+	public Docket swaggeConfiguration() {
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.app"))
+				.build();
+		
 	}
 
 }
