eabef8cf57032e87e2fb7643541348687ddf39ca
Anurag Singh <asingh72@binghamton.edu>
2021-10-24 22:01:54 -0400

    Removing test code

--- src/main/java/com/app/repository/UserRepositoy.java
+++ src/main/java/com/app/repository/UserRepositoy.java
@@ -9,12 +9,7 @@ import com.app.model.User;
 @Repository
 public interface UserRepositoy extends CrudRepository<User, Integer>{
 	
-	//boolean findByEmail(String email);
-	
 	@Query("select count(p) = 1 from User p where UPPER(email) = UPPER(?1)")
-    boolean findByEmail(String email);
+    	boolean findByEmail(String email);
 	
-//	@Query("update User set archivedAddress = ?1 where id = ?2")
-//    void updateArchivedAddress(Address address, int id);
-
 }
